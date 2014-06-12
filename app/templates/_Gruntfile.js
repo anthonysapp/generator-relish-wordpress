4b9eac67c11d9772cf1c7001eece5337e3e19f05
Anthony Sapp <anthony@Venom-2.local>
2014-06-12 16:02:46 -0400

    added grunt tasks pngmin, spritesmith

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -7,15 +7,15 @@ module.exports = function(grunt) {
             },
             js: {
                 src: [
-                    'assets/js/vendor/jquery-1.11.0.min.js',
+                    'src/js/vendor/jquery-1.11.0.min.js',
                     'bower_components/bootstrap/dist/js/bootstrap.min.js',
-                    'assets/js/vendor/jquery.validate.min.js',
-                    'assets/js/vendor/jrespond.min.js',
-                    'assets/js/vendor/owl.carousel.min.js',
-                    'assets/js/vendor/ScrollToPlugin.min.js',
-                    'assets/js/vendor/TweenMax.min.js',
-                    'assets/js/vendor/waypoints.min.js',
-                    'assets/js/main.js'
+                    'src/js/vendor/jquery.validate.min.js',
+                    'src/js/vendor/jrespond.min.js',
+                    'src/js/vendor/owl.carousel.min.js',
+                    'src/js/vendor/ScrollToPlugin.min.js',
+                    'src/js/vendor/TweenMax.min.js',
+                    'src/js/vendor/jquery.inview.js',
+                    'src/js/main.js'
                 ],
                 dest: 'assets/js/<%= pkg.name %>.js'
             }
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'assets/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+                    'src/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
                 }
             }
         },
@@ -45,11 +45,11 @@ module.exports = function(grunt) {
         less: {
             development: {
                 options: {
-                    paths: ["css"],
+                    paths: ["less"],
                     cleancss: false
                 },
                 files: {
-                    "assets/css/<%= pkg.name %>.css": "assets/css/main.less"
+                    "assets/css/<%= pkg.name %>.css": "assets/less/main.less"
                 }
             },
             production: {
@@ -58,13 +58,76 @@ module.exports = function(grunt) {
                     cleancss: true
                 },
                 files: {
-                    "assets/css/<%= pkg.name %>.min.css": "assets/css/main.less"
+                    "assets/css/<%= pkg.name %>.min.css": "assets/less/main.less"
                 }
             }
         },
+        sprite: {
+            all: {
+                // Sprite files to read in
+                src: ['src/img/sprite/*'],
+
+                // Location to output spritesheet
+                destImg: 'src/img/sprite.png',
+
+                // Stylus with variables under sprite names
+                destCSS: 'src/less/sprite.less',
+
+                // OPTIONAL: Manual override for imgPath specified in CSS
+                imgPath: '../img/sprite.png',
+
+                // OPTIONAL: Specify algorithm (top-down, left-right, diagonal [\ format],
+                // alt-diagonal [/ format], binary-tree [best packing])
+                // Visual representations can be found below
+                algorithm: 'binary-tree',
+
+                // OPTIONAL: Specify padding between images
+                padding: 2,
+
+                // OPTIONAL: Specify engine (auto, phantomjs, canvas, gm, pngsmith)
+                engine: 'auto',
+
+                // OPTIONAL: Specify CSS format (inferred from destCSS' extension by default)
+                // (stylus, scss, scss_maps, sass, less, json, json_array, css)
+                cssFormat: 'less',
+
+
+
+                // OPTIONAL: Specify settings for algorithm
+                algorithmOpts: {
+                    // Skip sorting of images for algorithm (useful for sprite animations)
+                    //sort: false
+                },
+
+                // OPTIONAL: Specify css options
+                cssOpts: {
+                    // Some templates allow for skipping of function declarations
+                    functions: true,
+
+                    // CSS template allows for overriding of CSS selectors
+                    cssClass: function (item) {
+                        return '.sprite-' + item.name;
+                    }
+                }
+            }
+        },
+        pngmin: {
+            compile: {
+                options: {
+                    ext: '.png',
+                    force: true
+                },
+                files: [
+                    {
+                        src: 'src/img/sprite.png',
+                        dest: 'assets/img/'
+                    }
+                ]
+            }
+        },
         watch: {
-            files: ['Gruntfile.js','assets/css/*.less', 'assets/js/vendor/*.js','assets/js/main.js'],
-            tasks: ['build'],
+            files: ['Gruntfile.js','src/less/*.less', 'src/js/vendor/*.js','src/js/main.js'],
+            tasks: ['default'],
             options: {
                 livereload: true
             }
@@ -76,9 +139,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-pngmin');
 
     grunt.registerTask('test', ['jshint', 'concat']);
     grunt.registerTask('default', ['jshint','less:development', 'concat']);
-    grunt.registerTask('build', ['jshint', 'less', 'concat', 'uglify']);
+    grunt.registerTask('build', ['jshint', 'sprite','pngmin', 'less', 'concat', 'uglify']);
     grunt.registerTask('cssonly', ['less:development']);
 };
